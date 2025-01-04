f90bf799b0ca5034146156c287a45d687d703b00
djfox90 <djfox232@gmail.com>
2025-01-03 20:40:10 -0600

    add list of names, score and links

--- main.py
+++ main.py
@@ -8,13 +8,33 @@ def top_movie():
     }
     imdb_page = requests.get(IMDB_URL, headers=headers)
     
-    print(IMDB_URL)
-    print(imdb_page)
+    
     
     imdb_top_movies = BeautifulSoup(imdb_page.content, "html.parser")
     imdb_top_movies = BeautifulSoup(imdb_top_movies.prettify(), "html.parser")
+    
     movie_titles = imdb_top_movies.find_all("h3")
-    print(movie_titles[1].get_text())
+    movie_score = imdb_top_movies.find_all("span", class_="ipc-rating-star--rating")
+    movie_link = imdb_top_movies.find_all("a", class_="ipc-title-link-wrapper")
+   
+    movie_list_name=[]
+    movie_list_score=[]
+    movie_list_link=[]
+    for i in range(1,21):
+        
+        movie_list_name.append(movie_titles[i].get_text().strip())
+        movie_list_score.append(movie_score[i-1].get_text().strip())
+        link=movie_link[i].get('href')
+        link="https://www.imdb.com/"+link
+        movie_list_link.append(link)
+        print(link)
+       
+    print(movie_list_name)
+    print(movie_list_score)
+    print(movie_list_link)
+    
+    
+        
     
 top_movie()
     
