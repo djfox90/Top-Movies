3f08defe3530846e44619714bdb261b33cf539f4
djfox90 <djfox232@gmail.com>
2025-01-08 19:39:28 -0600

    Create df with data take from site

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
+from datetime import datetime
 
 def top_movie():
     IMDB_URL = "https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm"
@@ -19,16 +21,55 @@ def top_movie():
    
     movie_list_name=[]
     movie_list_score=[]
-    movie_list_link=[]
-    for i in range(1,21):
+    keys =["Action" , "Drama", "Holiday", "Romance", "Fantasy", "Musical","Horror"]
+    movie_dict =  {key: False for key in keys}
+    action_list =[]
+    drama_list = []
+    holiday_list = []
+    romance_list = []
+    fantasy_list = []
+    musical_list = []
+    horror_list = []
+       
+    for i in range(1,6):
         
         movie_list_name.append(movie_titles[i].get_text().strip())
         movie_list_score.append(movie_score[i-1].get_text().strip())
-        link=movie_link[i].get('href')
+        link=movie_link[i-1].get('href')
         link="https://www.imdb.com/"+link
-        movie_list_link.append(link)
         
        
+        MOVIE_URL = link
+        movie_page = requests.get(MOVIE_URL, headers=headers)
+        imdb_movies = BeautifulSoup(movie_page.content, "html.parser")
+        imdb_movies = BeautifulSoup(imdb_movies.prettify(), "html.parser")
+        genres = imdb_movies.find_all("span", class_="ipc-chip__text")
+        
+        movie_dict =  {key: False for key in keys}
+        for i in range(len(genres)-1):
+            
+            if genres[i].get_text().strip() in movie_dict:
+                movie_dict[genres[i].get_text().strip()] = True
+        print(movie_dict)
+        
+        action_list.append(movie_dict["Action"]) 
+        drama_list.append(movie_dict["Drama"])
+        holiday_list.append(movie_dict["Holiday"])
+        romance_list.append(movie_dict["Romance"])
+        fantasy_list.append(movie_dict["Fantasy"])
+        musical_list.append(movie_dict["Musical"])
+        horror_list.append(movie_dict["Horror"])
+   
+    
+    data = {"Titles": movie_list_name, "Score": movie_list_score, "Action": action_list, "Drama": drama_list,"Holiday": holiday_list, "Romance":romance_list, "Fantasy":fantasy_list, "Musical":musical_list,"Horror":horror_list}
+    df = pd.DataFrame(data)
+    df['Rating'] = range(1, len(df) + 1)
+    date = datetime.now()
+    month = date.strftime("%B")
+    df['Month'] = month
+    
+    print(df)
+           
    
     
     
@@ -43,9 +84,20 @@ def movie_genere():
     imdb_movies = BeautifulSoup(movie_page.content, "html.parser")
     imdb_movies = BeautifulSoup(imdb_movies.prettify(), "html.parser")
     
+    keys = ["Romance", "Musical", "Fantasy", "Horror"]
+    movie_dict =  {key: False for key in keys}
+    print(movie_dict)
+    g_list = []
     genres = imdb_movies.find_all("span", class_="ipc-chip__text")
-    print(genres[1].get_text())
-movie_genere()
+    for i in range(len(genres)-1):
+        if genres[i].get_text().strip() in movie_dict:
+            movie_dict[genres[i].get_text().strip()] = True
+    print(movie_dict)
+    g_list.append(movie_dict["Horror"])
+    g_list.append(movie_dict["Musical"])
+    print(g_list)
+            
+top_movie()
     
     
     
